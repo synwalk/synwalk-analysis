813951d0f01f7fab1ad6e3fefd25cbb4dff09093
ctoth <christian.toth@tugraz.at>
2020-07-15 15:43:58 +0200

    feat: run LFR benchmarks as a function of the network size
    Adapt & expand the existing codebase to allow the generation, storage
    and evaluation of LFR benchmarks where the variable is the network size.
    This is in contrast to varying the mixing parameter as supported
    previously.

--- src/lfr/run_benchmark.py
+++ src/lfr/run_benchmark.py
@@ -26,18 +26,19 @@ def run_benchmark(benchmark_dir, results_dir, clustering_method, max_samples=100
     Parameters
     ----------
     benchmark_dir : str
-        Path to a directory containing subdirectories named according to <xx>mu. The subdirectories
-        contain benchmark graphs where <xx> represents the first two decimals of their mixing parameter.
+        Path to a directory containing a set of benchmark graphs, ordered in subdirectories.
+        E.g. ´benchmark_dir = ../data/lfr_benchmark/15deg/300n/´.
     results_dir : str
-        Path to a directory where to store the resulting clusterings in subdirectories reflecting the directory
-        structure in benchmark_dir.
+        Path to a directory where to store the resulting clusterings. These will be stored in subdirectories reflecting
+        the directory structure in benchmark_dir.
+        E.g. ´results_dir = ../results/lfr_benchmark/clustering/infomap/15deg/300n/´.
     clustering_method : str
         String identifier for a clustering method that should be applied to the benchmark graphs. Available clustering
         methods are 'infomap', 'synwalk', 'walktrap' and 'label_propagation'.
     max_samples : int
-        Maximum number of benchmark graphs processed from each subdirectory.
+        Maximum number of benchmark graphs processed from each subdirectory in ´benchmark_dir´.
     overwrite_results : bool
-        Overwrites existing result files if True. Skips evaluation if False.
+        Overwrites existing clustering files if True. Skips evaluation if False.
     """
     # check if clustering method is valid
     if clustering_method not in methods:
@@ -50,8 +51,8 @@ def run_benchmark(benchmark_dir, results_dir, clustering_method, max_samples=100
     folders = [entry for entry in os.scandir(benchmark_dir) if entry.is_dir()]
     for folder_idx, folder in enumerate(folders):
         # create results storage dir if necessary
-        mu_dir = results_dir + os.path.basename(folder) + '/'
-        os.makedirs(mu_dir, exist_ok=True)
+        var_dir = results_dir + os.path.basename(folder) + '/'
+        os.makedirs(var_dir, exist_ok=True)
 
         # iterate over graph files and evaluate
         graph_files, _ = get_benchmark_files(folder)
@@ -61,7 +62,7 @@ def run_benchmark(benchmark_dir, results_dir, clustering_method, max_samples=100
                 break
 
             # assemble results file path and check existence
-            clu_path = mu_dir + 'clustering_' + str(seed) + '_' + clustering_method + '.json'
+            clu_path = var_dir + 'clustering_' + str(seed) + '_' + clustering_method + '.json'
             if not overwrite_results and os.path.exists(clu_path):
                 print('\rResults in ' + clu_path + ' already exist. Skipping evaluation...')
                 continue
@@ -74,21 +75,25 @@ def run_benchmark(benchmark_dir, results_dir, clustering_method, max_samples=100
     print('')
 
 
-def evaluate_clustering_results(benchmark_dir, results_dir, metric='ami') -> BenchmarkResults:
-    """Run a given clustering method on LFR benchmark graphs.
-
-    We assume benchmark graphs with a fixed network size and variable mixing parameter mu.
+def evaluate_clustering_results(benchmark_dir, results_dir, metric='ami', variable='mu') -> BenchmarkResults:
+    """Run a given clustering method on a set of LFR benchmark graphs.
 
     Parameters
     ----------
     benchmark_dir : str
-        Path to a directory containing subdirectories named according to <xx>mu. The subdirectories
-        contain ground truth clusterings where <xx> represents the first two decimals of their mixing parameter.
+        Path to a directory containing a set of ground truth clusterings, ordered in subdirectories.
+        E.g. ´benchmark_dir = ../data/lfr_benchmark/15deg/300n/´. The subdirectories are named according to ´<x>mu/´ or
+        ´<x>n/´, where <x> are the first two decimal digits of the mixing parameter in the first case. In the latter
+        case, <x> is the network size.
     results_dir : str
-        Path to a directory with the same structure as benchmark_dir containing the clusterings to be evaluated.
-    metric : callable
-        Clustering metric used to compare the clustering results to the ground truth clustering. The function
-        must accept two clusim Clustering objects as arguments.
+        Path to a directory with the same structure as benchmark_dir containing the predicted clusterings.
+        E.g. ´results_dir = ../results/lfr_benchmark/clustering/infomap/15deg/300n/´.
+    metric : str
+        Clustering metric used to compare the predicted and the ground truth clusterings. Available metrics are 'ami'
+        and 'synwalk_error'.
+    variable : str
+        The variable parameter of the benchmark set reflected in the subdirectory naming, i.e. 'n' for the network size
+        or 'mu' for the mixing parameter.
 
     Returns
     ------
@@ -99,20 +104,25 @@ def evaluate_clustering_results(benchmark_dir, results_dir, metric='ami') -> Ben
         print('Invalid metric - cancelling evaluation.')
         return BenchmarkResults()
 
+    if variable not in {'mu', 'n'}:
+        print('Invalid variable - cancelling evaluation.')
+        return BenchmarkResults()
+
     metric = metrics[metric]
     results = BenchmarkResults()
 
     # iterate over subfolders in the benchmark directory
     folders = [entry for entry in os.scandir(results_dir) if entry.is_dir()]
     for folder_idx, folder_pred in enumerate(folders):
-        mu = float(os.path.basename(folder_pred)[0:2]) / 100
-        folder_true = benchmark_dir + os.path.basename(folder_pred) + '/'
+        folder_name = os.path.basename(folder_pred)
+        var = float(folder_name[0:2]) / 100 if variable == 'mu' else int(folder_name[:-1])
+        folder_true = benchmark_dir + folder_name + '/'
 
         # get clustering file paths
         _, clu_files_pred = get_benchmark_files(folder_pred)
         graph_files, clu_files_true = get_benchmark_files(folder_true)
         # iterate over files and evaluate
-        dp = DataPoint(mu, len(clu_files_pred))
+        dp = DataPoint(var, len(clu_files_pred))
         for seed, clu_file_pred in clu_files_pred.items():
             assert (seed in clu_files_true)  # there should be a ground truth
             score = metric(clu_file_pred, clu_files_true[seed], graph_files[seed])
